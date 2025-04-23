f9651194c67301fe499a861e0856d4b8d88f432d
sfk <1416967786@qq.com>
2025-04-24 00:06:48 +0800

    passed lab2 tests

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,11 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimesAllPrime() {
+        IntList l = IntList.of(1, 3, 5, 7, 11);
+        assertTrue(IntListExercises.squarePrimes(l));
+        assertEquals("1 -> 9 -> 25 -> 49 -> 121", l.toString());
+    }
 }
